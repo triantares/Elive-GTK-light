d9b2512e286a31eda661df765c7ab600d54b91a4
triantares <triantares@gmail.com>
2019-08-21 13:36:26 +0200

    finished install.sh file

--- Install-menus/install.sh
+++ Install-menus/install.sh
@@ -1,16 +1,6 @@
-#! /usr/bin/bash
-
-#Copy the files in the "menus-general/" folder to ~.e16/menus
-#This is an ugly hack I know but will serve it's putpose untill I find out how to set that overall.
-OPTIONS="Options:
-  -h, --help  This help menu
-  -p, --persistant  Have the menu reloaded at every new e16 login 
-  
-while true ; do
-  case "$1" in
-    -h|--help)
-       printf "Usage: $(basename $0) [options]\n\n$OPTIONS\n\n" ; exit 1 ;;
-     -p|--persistant) cp -r menus-general/ ~/.e16/;echo "cp -t ~/.e16/menus-general/* ~/.e16/menus">> ~/.e16/startup-applications.list
-   esac
-done
-cp -r menus-general/* ~/.e16/menus
+read -n1 -p "Do you wnat a persistent fancy menu? then chose Y [y,n]" doit 
+case $doit in  
+  y) cp -r menus-general/ ~/.e16/;echo "cp -r ~/.e16/menus-general/* ~/.e16/menus">> ~/.e16/startup-applications.list ;; 
+  n) cp -r menus-general/* ~/.e16/menus ;; 
+  *) echo No choice made: exiting ;; 
+esac
