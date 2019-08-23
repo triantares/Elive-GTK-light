eff3183becb194ee40807f372f6976579a5c4dc6
triantares <triantares@gmail.com>
2019-08-23 09:09:25 +0200

    fixed install.sh

--- fancy-menus/install.sh
+++ fancy-menus/install.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/bash
 read -n1 -p "Do you wnat a persistent fancy menu? then chose Y [y,n]" doit 
 case $doit in  
-  y) cp -r fancy-menus/ ~/.e16/; echo "cp -r ~/.e16/fancy-menus/* ~/.e16/menus">> ~/.e16/startup-applications.list; cp fancy-menus/* ~/.e16/menus ;; 
+  y) cp -r fancy-menus ~/.e16/; echo "cp -r ~/.e16/fancy-menus/* ~/.e16/menus">> ~/.e16/startup-applications.list; cp fancy-menus/* ~/.e16/menus ;; 
   n) cp -r fancy-menus/* ~/.e16/menus ;; 
   *) echo No choice made: exiting ;; 
 esac
