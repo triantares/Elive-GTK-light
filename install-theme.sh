5604844a96ca2d4a33b3335cadc60ff3d1fcf366
triantares <triantares@gmail.com>
2019-08-23 09:25:32 +0200

    fixed install-*.sh

--- install-theme.sh
+++ install-theme.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/bash
 read -n1 -p "Do you wnat a persistent system wide them? then chose S, for use as user theme for yourself only chose U [s,u]" doit 
 case $doit in  
-  s) sudo cp -r `ls -A ../Elive-GTK-light |grep -Ev 'install.sh|ABOUT|LICENSE|README.md|.git'` /usr/share/e16/themes;; 
-  u) cp -r `ls -A ../Elive-GTK-light |grep -Ev 'install.sh|ABOUT|LICENSE|README.md|.git'` ~/.e16/themes ;; 
+  s) sudo cp -r `ls -A ../Elive-GTK-light |grep -Ev 'install.sh|ABOUT|LICENSE|README.md|.git'` /usr/share/e16/themes/Elive-GTK-light;; 
+  u) cp -r `ls -A ../Elive-GTK-light |grep -Ev 'install.sh|ABOUT|LICENSE|README.md|.git'` ~/.e16/themes/Elive-GTK-light ;; 
   *) echo No choice made: exiting ;; 
 esac
