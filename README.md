9bb3d68b61932980736b88a5c05c86996c36d974
triantares <triantares@gmail.com>
2019-09-29 16:38:33 +0200

    changed some minor stuff in reference to Install.sh

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Elive-GTK-light Theme
 A light GTK+ based theme for enlightenment E16 (DR16) specifically for Elive 3.7.x
 Based on the GTK+ theme by James M. Cape.
-For installation run the "install-theme.sh" file in this directory.
+For installation run the "install-theme.sh" file in this directory.....Do not give the installer root/sudo privileges, that will be asked for if needed.
--- install-theme.sh
+++ install-theme.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/bash
-read -n1 -p "Do you wnat a persistent system wide them? then chose S, for use as user theme for yourself only chose U [s,u]" doit 
+read -n1 -p "Do you wnat a persistent system wide them? then chose S, for use as user theme for yourself only (this is advisable and more secure) chose U [s,u]" doit 
 case $doit in  
   s) sudo mkdir /usr/share/e16/themes/Elive-GTK-light ; sudo cp -r `ls -A |grep -Ev 'install.sh|ABOUT|LICENSE|README.md|.git'` /usr/share/e16/themes/Elive-GTK-light;; 
   u) mkdir ~/.e16/themes/Elive-GTK-light ; cp -r `ls -A |grep -Ev 'install.sh|ABOUT|LICENSE|README.md|.git'` ~/.e16/themes/Elive-GTK-light ;; 
